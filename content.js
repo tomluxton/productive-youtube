1f7668a2ef66e61af3a2e8ec86b61e8880062df7
tomluxton <thomas.luxton@yahoo.com.au>
2020-01-02 15:53:30 +1000

    Fixed up some commenting and added to todo list

--- content.js
+++ content.js
@@ -6,11 +6,22 @@ console.log(tabURL);
 
 // Check url. Define variable as the element i want to remove
 // then use set its style to invisable
+	var tabURL = window.location.href;
+	function disable_suggested() {
+		if (tabURL == 'https://www.youtube.com/') {
+			var app_drawer = document.getElementsByTagName('ytd-two-column-browse-results-renderer');
+			// app_drawer[0].style.visibility = 'hidden';
+			for (i = 0; i < app_drawer.length; i++) {
+				app_drawer[i].style.visibility = 'hidden';
+			}
+		} else if (tabURL.startsWith('https://www.youtube.com/watch')){
+			var related_section = document.getElementById('related');
+			for (i = 0; i < related_section.length; i++) {
+				related_section[i].style.visibility = 'hidden';
+			}
+			// comment_section[0].style.visibility = 'hidden';
+		}
+	}
 
-if (tabURL == 'https://www.youtube.com/') {
-	var app_drawer = document.getElementsByTagName('ytd-rich-grid-renderer');
-	app_drawer[0].style.visibility = 'hidden';
-} else if (tabURL.startsWith('https://www.youtube.com/watch')){
-	var comment_section = document.getElementsByTagName('ytd-watch-next-secondary-results-renderer');
-	comment_section[0].style.visibility = 'hidden';
-}
+	disable_suggested();
+
